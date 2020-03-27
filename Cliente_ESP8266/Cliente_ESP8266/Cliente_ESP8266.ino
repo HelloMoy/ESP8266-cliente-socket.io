6c5328d1a5b3678e1368e02cac50b9bf077eddd0
Moises Astudillo <bruma_moy@hotmail.com>
2020-03-27 01:29:57 -0600

    Envio de datos Json al servidor

--- Cliente_ESP8266/Cliente_ESP8266/Cliente_ESP8266.ino
+++ Cliente_ESP8266/Cliente_ESP8266/Cliente_ESP8266.ino
@@ -8,6 +8,10 @@
 ESP8266WiFiMulti WiFiMulti;
 SocketIoClient webSocket;
 
+int periodo = 1000; //periodo del envio de datos
+unsigned long TiempoAhora = 0;
+int temp = 17;
+
 void event(const char * payload, size_t length) {
   Serial.println("Dato recibido: " + String((char*)payload));
   if(String((char*)payload)=="Hola"){
@@ -17,13 +21,14 @@ void event(const char * payload, size_t length) {
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Conectado");
+  Serial.println("Iniciando");
 
     WiFiMulti.addAP("INFINITUMr7tf", "910c52701e");
 
     while(WiFiMulti.run() != WL_CONNECTED) {
         delay(100);
     }
+
 
     webSocket.on("saludo", event);
     webSocket.emit("alerta", "\"Alerta desde ESP8266\"");
@@ -32,4 +37,12 @@ void setup() {
 
 void loop() {
     webSocket.loop();
+    if(millis() > TiempoAhora + periodo){
+      String dato = "{R:90, G:120, B:255}";                         //Dato a enviar
+      String formato = "\"" + dato + "\"" ;
+      const char *casteo = const_cast<char*>(formato.c_str());      //Casteo del dato a enviar
+      webSocket.emit("alerta", casteo );                            //Envio de datos al servidor
+
+      TiempoAhora = millis();
+    }
 }
